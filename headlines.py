843122d1d5c19adf28a3e4324e96b2c23fcaebc6
Willie <mulwawillie@gmail.com>
2016-10-16 11:47:11 +0300

    POST requests

--- headlines.py
+++ headlines.py
@@ -5,10 +5,10 @@ from flask import request
 
 app = Flask (__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml','cnn':'http://rss.cnn.com/rss/edition.rss','fox': 'http://feeds.foxnews.com/foxnews/latest','ioL': 'http://www.ioL.co.za/cmlink/1.640'}
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 @app.route("/<publication>")
 def get_news():
-    query = request.args.get("publication")
+    query = request.form.get("publication")
     if not query or query.lower() not in RSS_FEEDS:
         publication = "bbc"
     else:
