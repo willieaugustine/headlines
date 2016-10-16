9c05f17f89c6e6d77fae96158cff0924ef304c8e
Willie <mulwawillie@gmail.com>
2016-10-16 12:45:44 +0300

    adding weather app

--- headlines.py
+++ headlines.py
@@ -2,6 +2,9 @@ import feedparser
 from flask import Flask
 from flask import render_template
 from flask import request
+import json
+import urllib2
+import urllib
 
 app = Flask (__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml','cnn':'http://rss.cnn.com/rss/edition.rss','fox': 'http://feeds.foxnews.com/foxnews/latest','ioL': 'http://www.ioL.co.za/cmlink/1.640'}
@@ -14,7 +17,22 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
-    return render_template("home.html", articles=feed['entries'])
+    weather = get_weather("London, UK")
+    return render_template("home.html", articles=feed['entries'], weather=weather)
+def get_weather(query):
+    api_url = http://api.openweathermap.org/data/2.5/
+    weather?q={}&units=metric&appid=<c01b8760dd45f3620b131ed32b24dfc3>
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description":
+                   parsed["weather"][0]["description"],
+                   "temperature":parsed["main"]["temp"],"city":parsed["name"]}
+        return weather
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
