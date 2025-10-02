032bb1af5c0e2e1e04815cd07d518b813c22fefb
Willie <mulwawillie@gmail.com>
2025-10-02 15:08:55 +0300

    Update headlines.py

--- headlines.py
+++ headlines.py
@@ -8,9 +8,6 @@ import urllib
 import datetime
 from flask import make_response
 
-
-WEATHER_URL = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c01b8760dd45f3620b131ed32b24dfc3'
-CURRENCY_URL = 'https://openexchangerates.org//api/latest.json?app_id=10cb38788d5e4083bbb1ee0375f87786'
 app = Flask (__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml','cnn':'http://rss.cnn.com/rss/edition.rss','fox': 'http://feeds.foxnews.com/foxnews/latest','ioL': 'http://www.ioL.co.za/cmlink/1.640'}
 # ...
